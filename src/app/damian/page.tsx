b38a249fc89351c96b6ecbb8fc9532d0054b8c5a
hancockdamian <damian.hancock09@gmail.com>
2025-08-15 12:46:43 +0000

    feat: pixel trail

--- src/app/damian/page.tsx
+++ src/app/damian/page.tsx
@@ -1,9 +1,25 @@
-export default function DamianHome() {
-    return (
-      <main className="flex items-center justify-center min-h-screen bg-gray-300 w-full p-6">
+"use client";
+
+import PixelTrail from "@/components/PixelTrail";
+
+export default function Page() {
+  return (
+    <main className="relative w-screen h-screen overflow-hidden bg-gray-300">
+      {/* Fullscreen PixelTrail */}
+      <PixelTrail
+        gridSize={50}
+        trailSize={0.05}
+        maxAge={150}
+        interpolate={3}
+        color="rgb(236, 15, 177)"
+        gooeyFilter={{ id: "custom-goo-filter", strength: 0.5 }}
+      />
+
+      <div className="absolute inset-0 flex items-center justify-center">
         <section className="rounded-2xl p-6 text-black text-6xl text-center">
           Damian Hancock
         </section>
-      </main>
-    );
-  }
+      </div>
+    </main>
+  );
+}
